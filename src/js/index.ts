cba781d0f8355f624461fe03ef4c93bd0c178100
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-03-11 11:51:02 +0100

    fjernet kode som skal på loop branch

--- src/js/index.ts
+++ src/js/index.ts
@@ -17,14 +17,6 @@
  console.log(listeusund);
 
 
-//  Looper igennem alle elementer med tagname <li>, hvis class attributten er lig usundt
-//  Sættes den til sundt 
- let listeLi = document.getElementsByTagName('li');
- for (let i = 0; i < listeLi.length; i++) {
-     console.log('class : ' + listeLi[i].getAttribute('class'));
-     if (listeLi[i].getAttribute('class') == 'usundt')
-       listeLi[i].setAttribute('class','sundt');
-}
 
  
 
