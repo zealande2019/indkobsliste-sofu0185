ae049daa4d840a256daf711e60ebb6b5246b9335
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-03-11 11:45:49 +0100

    Eksempel på loop og ændring af attribute class på li element

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,10 +3,28 @@
 // elementNode.textContent = "Hallo";
 
  let listeusund = document.getElementById('listeUsund');
+ 
+ console.log(listeusund)
+ 
  let newLi = document.createElement('li');
- let newText = document.createTextNode('Sodavand')
  newLi.setAttribute('class','usundt');
-newLi.appendChild(newText);
+
+ let newText = document.createTextNode('Sodavand')
+ newLi.appendChild(newText);
+
  listeusund.appendChild(newLi);
 
  console.log(listeusund);
+
+
+//  Looper igennem alle elementer med tagname <li>, hvis class attributten er lig usundt
+//  Sættes den til sundt 
+ let listeLi = document.getElementsByTagName('li');
+ for (let i = 0; i < listeLi.length; i++) {
+     console.log('class : ' + listeLi[i].getAttribute('class'));
+     if (listeLi[i].getAttribute('class') == 'usundt')
+       listeLi[i].setAttribute('class','sundt');
+}
+
+ 
+
