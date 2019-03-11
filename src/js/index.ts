1573ef5e74110a65c77ae4b0417c5df3d7d47e72
sofu0185 <sofu0185@edu.easj.dk>
2019-03-11 15:06:31 +0100

    opgave løst

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,22 @@
 //Her skal du skrive din typescriptkode
+
+let liste: HTMLElement = document.getElementById('liste');
+
+document.getElementById('btnNyVare').addEventListener('click', function(){
+    let value: string = (<HTMLInputElement>document.getElementById('nyVareText')).value;
+    let nyVare: HTMLElement = document.createElement('li');
+    let type: string = (<HTMLInputElement>document.getElementById('typeNyVare')).value;
+
+    nyVare.setAttribute('id', GetUniqueId().toString());
+    nyVare.setAttribute('class', type);
+    nyVare.innerText = value;
+
+    liste.appendChild(nyVare);
+});
+
+
+function GetUniqueId(): number{
+    let lastItem: number = liste.children.length - 1;
+    let lastId: number = parseInt(liste.children[lastItem].getAttribute('id'));
+    return lastId + 1;
+}
