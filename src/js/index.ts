1608efadb1dfd45c5f298e771b0987588fc1d75e
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-03-11 11:54:27 +0100

    fjernet kode

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,22 +1,3 @@
-// let elementNode = document.getElementById('one');
-
-// elementNode.textContent = "Hallo";
-
- let listeusund = document.getElementById('listeUsund');
- 
- console.log(listeusund)
- 
- let newLi = document.createElement('li');
- newLi.setAttribute('class','usundt');
-
- let newText = document.createTextNode('Sodavand')
- newLi.appendChild(newText);
-
- listeusund.appendChild(newLi);
-
- console.log(listeusund);
-
-
 //  Looper igennem alle elementer med tagname <li>, hvis class attributten er lig usundt
 //  Sættes den til sundt 
  let listeLi = document.getElementsByTagName('li');
